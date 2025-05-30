53ff628f545add354f5f0f79ee7574c13c4547f5
Hassan <hassanmousavi400@gmail.com>
2025-05-30 11:53:40 +0330

    My first React challenge

--- src/index.js
+++ src/index.js
@@ -1,17 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+function Avatar() {
+  return (
+    <img className="avatar" src="avatar-pic/avatar.jpg" alt="Avatar-picture" />
+  );
+}
+
+function Intro() {
+  return (
+    <div className="data">
+      <h1>Hassan Mousavi</h1>
+      <p>
+        Front-end developer and a student who works on their projects very hard
+        and foucsly.I like mobile games for example Call of duty mobile and
+        Mobile Legend etc.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill
+        skillName="Html+Css"
+        icon="💪"
+        backgroundcolor={{ backgroundColor: "#0000ff" }}
+      />
+      <Skill
+        skillName="JavaScript"
+        icon="💪"
+        backgroundcolor={{ backgroundColor: "#ffff00" }}
+      />
+      <Skill
+        skillName="Web Design"
+        icon="💪"
+        backgroundcolor={{ backgroundColor: "#90ee90" }}
+      />
+      <Skill
+        skillName="Git and GitHub"
+        icon="👍"
+        backgroundcolor={{ backgroundColor: "#ff0000" }}
+      />
+      <Skill
+        skillName="React"
+        icon="💪"
+        backgroundcolor={{ backgroundColor: "#add8e6" }}
+      />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <p className="skill" style={props.backgroundcolor}>
+      {props.skillName} {props.icon}
+    </p>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
